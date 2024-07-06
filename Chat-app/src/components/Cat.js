6a470f4a251db8e7b0babb57c286cae908be7599
Fouzan M <alfouz236@gmail.com>
2024-07-06 23:23:55 +0530

    [UPDT] menu section on left side

---
+++ Chat-app/src/components/Cat.js
@@ -0,0 +1,10 @@
+import { Avatar } from '@mui/material'
+import React from 'react'
+
+const Cat = () => {
+  return (
+    <Avatar src='https://images.pexels.com/photos/842811/pexels-photo-842811.jpeg?auto=compress&cs=tinysrgb&w=600'/>
+  )
+}
+
+export default Cat;
