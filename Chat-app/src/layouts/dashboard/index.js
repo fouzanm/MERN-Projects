92a4b31ccb493b5d9b34aaf9cf8ead0a569ea72a
Fouzan M <alfouz236@gmail.com>
2024-07-07 01:18:11 +0530

    [UPDT] chat element added

--- Chat-app/src/layouts/dashboard/index.js
+++ Chat-app/src/layouts/dashboard/index.js
@@ -57,43 +57,45 @@ const DashboardLayout = () => {
   const {onToggleMode} = useSettings();
   return (
     <>
-      <Box p={2} sx={{backgroundColor: theme.palette.background.paper, boxShadow: "0px 0px 2px rgba(0, 0, 0, 0.25)", height:' 100vh', width: "100px"}}>
-        <Stack direction='column' alignItems={'center'} justifyContent={'space-between'} sx={{height: '100%'}} spacing={3}>
-          <Stack alignItems={'center'} spacing={4}>
-            <Box sx={{backgroundColor: theme.palette.primary.main, height: 64, width:64, borderRadius: 1.5}}>
-              <img src={Logo} alt=''/>
-            </Box>
-            <Stack sx={{width: 'max-content'}} direction={'column'} alignItems={'center'} spacing={3}>
-              {Nav_Buttons.map((el) =>  
-                el.index === selected ?
-                  <Box sx={{backgroundColor: theme.palette.primary.main, borderRadius: 1.5}} key={el.index} >
+      <Stack direction={'row'}>
+        <Box p={2} sx={{backgroundColor: theme.palette.background.paper, boxShadow: "0px 0px 2px rgba(0, 0, 0, 0.25)", height:' 100vh', width: "100px"}}>
+          <Stack direction='column' alignItems={'center'} justifyContent={'space-between'} sx={{height: '100%'}} spacing={3}>
+            <Stack alignItems={'center'} spacing={4}>
+              <Box sx={{backgroundColor: theme.palette.primary.main, height: 64, width:64, borderRadius: 1.5}}>
+                <img src={Logo} alt=''/>
+              </Box>
+              <Stack sx={{width: 'max-content'}} direction={'column'} alignItems={'center'} spacing={3}>
+                {Nav_Buttons.map((el) =>  
+                  el.index === selected ?
+                    <Box sx={{backgroundColor: theme.palette.primary.main, borderRadius: 1.5}} key={el.index} >
+                      <IconButton sx={{width: 'max-content', color: '#fff'}}>
+                          {el.icon}
+                      </IconButton>
+                    </Box>
+                    : <IconButton key={el.index} sx={{width: 'max-content', color:theme.palette.mode === 'light'? '#000': theme.palette.text.primary}} onClick={() => setSelected(el.index)}>
+                    {el.icon}
+                </IconButton>)}
+                <Divider sx={{width: "48px"}}/>
+                {selected === 3 ? 
+                  <Box sx={{backgroundColor: theme.palette.primary.main, borderRadius: 1.5}}>
                     <IconButton sx={{width: 'max-content', color: '#fff'}}>
-                        {el.icon}
+                      <Gear/>
                     </IconButton>
                   </Box>
-                  : <IconButton key={el.index} sx={{width: 'max-content', color:theme.palette.mode === 'light'? '#000': theme.palette.text.primary}} onClick={() => setSelected(el.index)}>
-                  {el.icon}
-              </IconButton>)}
-              <Divider sx={{width: "48px"}}/>
-              {selected === 3 ? 
-                <Box sx={{backgroundColor: theme.palette.primary.main, borderRadius: 1.5}}>
-                  <IconButton sx={{width: 'max-content', color: '#fff'}}>
-                    <Gear/>
-                  </IconButton>
-                </Box>
-                :
-              <IconButton sx={{width: 'max-content', color: theme.palette.mode === 'light'? '#000': theme.palette.text.primary}} onClick={() => setSelected(3)}>
-                <Gear/>
-              </IconButton>}
+                  :
+                <IconButton sx={{width: 'max-content', color: theme.palette.mode === 'light'? '#000': theme.palette.text.primary}} onClick={() => setSelected(3)}>
+                  <Gear/>
+                </IconButton>}
+              </Stack>
+            </Stack>
+            <Stack spacing={4}>
+              <AntSwitch defaultChecked onClick={() => onToggleMode()}/>
+              <Avatar src={faker.image.avatar()}/>
             </Stack>
           </Stack>
-          <Stack spacing={4}>
-            <AntSwitch defaultChecked onClick={() => onToggleMode()}/>
-            <Avatar src={faker.image.avatar()}/>
-          </Stack>
-        </Stack>
-      </Box>
-      <Outlet />
+        </Box>
+        <Outlet />
+      </Stack>
     </>
   );
 };
--- Chat-app/src/pages/dashboard/GeneralApp.js
+++ Chat-app/src/pages/dashboard/GeneralApp.js
@@ -1,13 +1,12 @@
 import React, {Suspense, lazy} from "react";
+import { Chats } from "./Chats";
 const Cat = lazy(() => import("../../components/Cat"));
 
 const GeneralApp = () => {
 
   return (
     <>
-      <Suspense fallback='Loading...'>
-        <Cat/>
-      </Suspense>
+      <Chats/>
     </>
   );
 };
