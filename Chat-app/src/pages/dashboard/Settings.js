168aadab3f4c34a45e27c54eb4ad3e2c3e98e772
fouzanm <alfouz236@gmail.com>
2024-07-31 20:22:04 +0530

    [ADD] shortcut dialog added

--- Chat-app/src/pages/dashboard/Settings.js
+++ Chat-app/src/pages/dashboard/Settings.js
@@ -20,6 +20,7 @@ import {
   Info,
 } from "phosphor-react";
 import { faker } from "@faker-js/faker";
+import Shortcuts from "../../sections/settings/Shortcuts";
 
 const Settings = () => {
   const theme = useTheme();
@@ -92,54 +93,63 @@ const Settings = () => {
   ];
 
   return (
-    <Stack direction={"row"} sx={{ width: "100%" }}>
-      {/* Left Panel */}
-      <Box
-        sx={{
-          overflowY: "auto",
-          height: "100vh",
-          width: 320,
-          backgroundColor:
-            theme.palette.mode === "light"
-              ? "#F8FAFF"
-              : theme.palette.background,
-          boxShadow: "0px 0px 2px rgba(0, 0, 0, 0.25)",
-        }}
-      >
-        <Stack p={4} spacing={5}>
-          <Stack direction={"row"} alignItems={"center"} spacing={3}>
-            <IconButton>
-              <CaretLeft size={24} color="#4B4B4B" />
-            </IconButton>
-            <Typography variant="h6">Settings</Typography>
-          </Stack>
-          <Stack direction={"row"} spacing={3}>
-            <Avatar
-              sx={{ width: 56, height: 56 }}
-              src={faker.image.avatar()}
-              alt={faker.name.fullName()}
-            />
-            <Stack spacing={0.5}>
-              <Typography variant="article">{faker.name.fullName()}</Typography>
-              <Typography variant="body2">{faker.random.words()}</Typography>
+    <>
+      <Stack direction={"row"} sx={{ width: "100%" }}>
+        {/* Left Panel */}
+        <Box
+          sx={{
+            overflowY: "auto",
+            height: "100vh",
+            width: 320,
+            backgroundColor:
+              theme.palette.mode === "light"
+                ? "#F8FAFF"
+                : theme.palette.background,
+            boxShadow: "0px 0px 2px rgba(0, 0, 0, 0.25)",
+          }}
+        >
+          <Stack p={4} spacing={5}>
+            <Stack direction={"row"} alignItems={"center"} spacing={3}>
+              <IconButton>
+                <CaretLeft size={24} color="#4B4B4B" />
+              </IconButton>
+              <Typography variant="h6">Settings</Typography>
+            </Stack>
+            <Stack direction={"row"} spacing={3}>
+              <Avatar
+                sx={{ width: 56, height: 56 }}
+                src={faker.image.avatar()}
+                alt={faker.name.fullName()}
+              />
+              <Stack spacing={0.5}>
+                <Typography variant="article">
+                  {faker.name.fullName()}
+                </Typography>
+                <Typography variant="body2">{faker.random.words()}</Typography>
+              </Stack>
+            </Stack>
+            <Stack spacing={4}>
+              {list.map(({ key, icon, title, onclick }) => (
+                <>
+                  <Stack
+                    spacing={2}
+                    sx={{ cursor: "pointer" }}
+                    onClick={onclick}
+                  >
+                    <Stack direction={"row"} spacing={2} alignItems={"center"}>
+                      {icon} <Typography variant="body2">{title}</Typography>
+                    </Stack>
+                  </Stack>
+                  {key !== 7 && <Divider />}
+                </>
+              ))}
             </Stack>
           </Stack>
-          <Stack spacing={4}>
-            {list.map(({ key, icon, title, onclick }) => (
-              <>
-                <Stack spacing={2} sx={{ cursor: "pointer" }} onClick={onclick}>
-                  <Stack direction={"row"} spacing={2} alignItems={"center"}>
-                    {icon} <Typography variant="body2">{title}</Typography>
-                  </Stack>
-                </Stack>
-                {key !== 7 && <Divider/>}
-              </>
-            ))}
-          </Stack>
-        </Stack>
-      </Box>
-      {/* Right Panel */}
-    </Stack>
+        </Box>
+        {/* Right Panel */}
+      </Stack>
+      <Shortcuts open={true} handleClose={() => {}}/>
+    </>
   );
 };
 
