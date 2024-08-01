091fdd71c36a3c0452689c3046e779c6b7282742
Fouzan M <alfouz236@gmail.com>
2024-08-01 09:00:42 +0530

    [UPDT] form

---
+++ Chat-app/src/components/hook-form/FormProvider.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { FormProvider as Form } from 'react-hook-form'
+const FormProvider = () => {
+  return (
+    <Form></Form>
+  )
+}
+
+export default FormProvider
---
+++ Chat-app/src/components/hook-form/RHFTextField.js
@@ -0,0 +1,34 @@
+import PropTypes from "prop-types";
+import { useFormContext, Controller } from "react-hook-form";
+import { TextField } from "@mui/material";
+
+RHFTextField.prototype = {
+  name: PropTypes.string,
+  helperText: PropTypes.node,
+};
+
+const RHFTextField = ({ name, helperText, ...other }) => {
+  const { control } = useFormContext();
+  return (
+    <Controller
+      name={name}
+      control={control}
+      render={({ field, fieldState: { error } }) => (
+        <TextField
+          {...field}
+          fullWidth
+          value={
+            typeof field.value === "number" && field.value === 0
+              ? ""
+              : field.value
+          }
+          error={!!error}
+          helperText={error ? error.message : helperText}
+          {...other}
+        />
+      )}
+    />
+  );
+};
+
+export default RHFTextField;
